82675e18bed1510f723a12b7a87314125e1bf744
juliusrugi <juliusrugiw@gmail.com>
2025-12-19 23:56:56 +0300

    Add files via upload

---
+++ ASSIGNMENT 3/TASK 4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+
+    int *ptr1;
+    int *ptr2;
+
+    ptr1 = &num1;
+    ptr2 = &num2;
+
+    int sum;
+
+    sum = *ptr1 + *ptr2;
+
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
